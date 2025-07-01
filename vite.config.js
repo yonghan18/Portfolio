214fc950b436dee132daf7eb3919f313e96906b5
Yong Han Lee <llee0093@student.monash.edu>
2025-07-01 10:15:00 +0800

    feat: add mkcert to run https locally - added ssl certificate to run https locally - added mkcert to generate ssl certificate - added vite.config.js to configure ssl certificate - added package-lock.json to lock the dependencies - added package.json to manage the dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    https: true,
+  },
   plugins: [
     react(),
     tailwindcss(),
+    mkcert(),
   ],
 })
