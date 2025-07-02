05719c5a74beb25de8287df1b6d48ca81e33d6ce
Yong Han Lee <llee0093@student.monash.edu>
2025-07-02 11:30:00 +0800

    feat: add personal projects details - for future work section usage - json format: {     "id": 1,     "title": "Project 1",     "description": "Description of project 1",     "image": "path/to/image.jpg",     "technologies": ["React", "Tailwind CSS"],     "liveUrl": "",     "sourceUrl": "",     "category": "Frontend" }

--- src/constants/index.js
+++ src/constants/index.js
@@ -18,3 +18,55 @@ export const navLinks = [
         url: '#contact'
     }
 ]
+export const projects = [
+    {
+        id: 1,
+        title: "Hiredly: Internal AI-Powered SEO Content Generator",
+        description: "A modern, responsive portfolio website built with React and Tailwind CSS. Features smooth animations, dynamic typing effects, and mobile-first design principles.",
+        image: "/assets/portfolio.png",
+        technologies: ["React", "Tailwind CSS", "n8n", "OpenAI API", "Grammarly API"],
+        liveUrl: "#",
+        sourceUrl: "#",
+        category: "Frontend"
+    },
+    {
+        id: 2,
+        title: "Portfolio Website",
+        description: "A modern, responsive portfolio website built with React and Tailwind CSS. Features smooth animations, dynamic typing effects, and mobile-first design principles.",
+        image: "/assets/portfolio.png",
+        technologies: ["React", "Tailwind CSS"],
+        liveUrl: "#",
+        sourceUrl: "#",
+        category: "Full Stack"
+    },
+    {
+        id: 3,
+        title: "Santorini Game",
+        description: "A responsive e-commerce platform with shopping cart functionality, payment integration, and admin dashboard. Implemented using modern web technologies.",
+        image: "/assets/portfolio.png",
+        technologies: ["Java", "Java Swing", "OOP", "Design Patterns", "GitLab"],
+        liveUrl: "#",
+        sourceUrl: "#",
+        category: "Frontend"
+    },
+    {
+        id: 4,
+        title: "Nutrition Tracker App",
+        description: "A responsive e-commerce platform with shopping cart functionality, payment integration, and admin dashboard. Implemented using modern web technologies.",
+        image: "/assets/portfolio.png",
+        technologies: ["Kotlin", "Jetpack Compose", "Retrofit", "RoomDB", "Gemini API", "MVVM"],
+        liveUrl: "#",
+        sourceUrl: "#",
+        category: "Frontend"
+    },
+    {
+        id: 5,
+        title: "Guitar Hero Game",
+        description: "A responsive e-commerce platform with shopping cart functionality, payment integration, and admin dashboard. Implemented using modern web technologies.",
+        image: "/assets/portfolio.png",
+        technologies: ["TypeScript", "RxJS", "MVC Architecture", "FRP"],
+        liveUrl: "#",
+        sourceUrl: "#",
+        category: "Frontend"
+    }
+]
